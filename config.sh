da21cd181ccc041234d8225aec70279b0146c9a7
Karthikeyan VK <karthikeyanvk.techie@gmail.com>
2021-05-09 11:39:00 +0530

    fourth

--- config.sh
+++ config.sh
@@ -1,14 +1,14 @@
 apt update && apt upgrade
 apt install python python2 -y
-pip2 install --upgrade pip2
-pip2 install wheel
-pip2 install asyncio
-pip2 install telethon
-pip2 install colorama
-pip2 install bs4
-pip2 install rsa
-pip2 install requests
-pip2 install pyaes
-pip2 install async_generator
+pip install --upgrade pip
+pip install wheel
+pip install asyncio
+pip install telethon
+pip install colorama
+pip install bs4
+pip install rsa
+pip install requests
+pip install pyaes
+pip install async_generator
 termux-setup-storage
 chmod 777 start.sh
